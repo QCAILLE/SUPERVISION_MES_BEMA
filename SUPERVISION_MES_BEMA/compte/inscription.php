80bc084d1951c9487e098e3dfbe1f35215ab302c
QCAILLE <48440775+QCAILLE@users.noreply.github.com>
2020-02-04 11:08:00 +0100

    Add files via upload

---
+++ SUPERVISION_MES_BEMA/compte/inscription.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Créer un compte</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+</head>
+<body>
+	<form method="POST" action="">
+		
+	</form>
+
+</body>
+</html>
